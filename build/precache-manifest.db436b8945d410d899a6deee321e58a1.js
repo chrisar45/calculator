1c22b2f6288c504122e56af0bd66e563546cd53b
Christian Ezequiel Fritzler <chrisar45@gmail.com>
2020-10-16 10:41:58 -0300

    deploy on firebase

---
+++ build/precache-manifest.db436b8945d410d899a6deee321e58a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a61a2b4f7e15b19c3a540d01b1c280",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13761f03f868112c42cc",
+    "url": "/static/css/main.e1048bdf.chunk.css"
+  },
+  {
+    "revision": "0a1900e319e38aeac1d9",
+    "url": "/static/js/2.78d878c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78d878c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13761f03f868112c42cc",
+    "url": "/static/js/main.fab46ebb.chunk.js"
+  },
+  {
+    "revision": "9ea45add3c82b2c9240b",
+    "url": "/static/js/runtime-main.9b0494e4.js"
+  }
+]);
